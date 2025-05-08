39edec97def6b7abae00265d70bd2b3188793545
catalinsorecau <catalin.sorecau@osudio.com>
2025-05-08 21:00:59 +0300

    Allow cors.

---
+++ src/main/java/com/github/utmb/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.github.utmb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all endpoints
+                        .allowedOrigins("*") // Allow all origins
+                        .allowedMethods("*") // Allow all HTTP methods
+                        .allowedHeaders("*"); // Allow all headers
+            }
+        };
+    }
+}
